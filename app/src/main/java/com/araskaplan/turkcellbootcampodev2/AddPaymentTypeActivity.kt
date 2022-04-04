6b3b1d645a6013ac85e4570dc020df3ee60ce245
Ömer Aras Kaplan <omeraraskaplan@gmail.com>
2022-04-04 19:28:37 +0300

    Activities and Designs added

---
+++ app/src/main/java/com/araskaplan/turkcellbootcampodev2/AddPaymentTypeActivity.kt
@@ -0,0 +1,11 @@
+package com.araskaplan.turkcellbootcampodev2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddPaymentTypeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_paymentType)
+    }
+}
